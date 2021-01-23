ab2b437e2239f5dd276a1e84b62e9762812d4f33
Zion Ayooki <zion.ayooki@gmail.com>
2021-01-24 01:47:29 +0200

    Added the footer and some styling

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,63 +1,93 @@
 import React from "react";
 import LogoLink from "../logo/LogoLink";
+import moment from "moment";
 
 class Footer extends React.Component {
   render() {
     return (
       <footer id="site-foot">
         <div className="container">
-          <LogoLink />
-          <div className="row align-items-start justify-content-between footer-info">
-            <div className="col-12 col-sm-6 col-md-4 mb-3">
-              <h2>Company</h2>
-              <ul>
-                <li>
-                  <a href="#">About us</a>
-                </li>
-                <li>
-                  <a href="#">Services</a>
-                </li>
-                <li>
-                  <a href="#">Partners</a>
-                </li>
-                <li>
-                  <a href="#">Career at Star House</a>
-                </li>
-                <li>
-                  <a href="#">Site map</a>
-                </li>
+          <div className="d-flex justify-content-center mb-3">
+              <LogoLink />
+          </div>
+          <div className="row align-items-start justify-content-between ps-4 ps-sm-0">
+            <div className="col-12 col-lg-6 mb-3">
+              <h2 className="footer-heading">Contact</h2>
+              <div className="container-fluid px-0">
+                <div className="row">
+                  <div className="col-12 col-sm-6 d-flex flex-column">
+                    <a className="footer-link" href="mailto:example@email.com">
+                      <i className="fas fa-envelope" aria-hidden={true}></i>
+                      <span>example@email.com</span>
+                    </a>
+                    <a className="footer-link" href="tel:031234567">
+                      <i className="fas fa-phone-alt" aria-hidden={true}></i>
+                      <span>03-1234567</span>
+                    </a>
+                    <div className="footer-text">
+                      <i className="fas fa-map-marker-alt" aria-hidden={true}></i>
+                      <div>
+                        <div>123rd Default Street,</div>
+                        <div>Tel Aviv</div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-sm-6">
+                    <div className="footer-text">
+                      <i className="fas fa-clock" aria-hidden={true}></i>
+                      <div>
+                        <div>Sunday - Thursday:</div>
+                        <div className="ps-3">08:00 - 17:00</div>
+                        <div>Friday:</div>
+                        <div className="ps-3">08:00 - 13:00</div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-12 col-sm-6 col-lg-3 mb-3">
+              <h2 className="footer-heading">Company</h2>
+              <ul className="footer-links-group" role="presentation">
+                <li role="presentation"><a className="footer-link" href="#">About us</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Services</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Partners</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Careers</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Site map</a></li>
               </ul>
             </div>
-            <div className="col-12 col-sm-6 col-md-4 mb-3">
-              <h2>Legal</h2>
-              <ul>
-                <li>
-                  <a href="#">Terms of use</a>
-                </li>
-                <li>
-                  <a href="#">Privacy policy</a>
-                </li>
-                <li>
-                  <a href="#">Accessibility</a>
-                </li>
+            <div className="col-12 col-sm-6 col-lg-3 mb-3">
+              <h2 className="footer-heading">Legal</h2>
+              <ul className="footer-links-group" role="presentation">
+                <li role="presentation"><a className="footer-link" href="#">Terms of use</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Privacy policy</a></li>
+                <li role="presentation"><a className="footer-link" href="#">Accessibility</a></li>
               </ul>
-            </div>
-            <div className="col-12 col-sm-6 col-md-4 mb-3">
-              <h2>Contact</h2>
-              <div>Email</div>
-              <div>Phone</div>
-              <div>Address</div>
-              <div>Working hours</div>
             </div>
           </div>
           <div className="row">
-            <div className="col-12">
-              Social links
+            <div className="col-12 footer-social">
+              <a className="social-link" href="#">
+                <i className="fab fa-facebook" aria-hidden={true}></i>
+                <span className="visually-hidden">Follow us on facebook</span>
+              </a>
+              <a className="social-link" href="#">
+                <i className="fab fa-twitter" aria-hidden={true}></i>
+                <span className="visually-hidden">Follow us on twitter</span>
+              </a>
+              <a className="social-link" href="#">
+                <i className="fab fa-instagram" aria-hidden={true}></i>
+                <span className="visually-hidden">Follow us on instagram</span>
+              </a>
+              <a className="social-link" href="#">
+                <i className="fab fa-whatsapp" aria-hidden={true}></i>
+                <span className="visually-hidden">Follow us on whatsApp</span>
+              </a>
             </div>
           </div>
           <div className="row">
             <div className="col-12 text-center footer-copy">
-              &copy; company name 2020. all rights reserved.
+              StarHouse &copy; 2000 - {moment().year()}
             </div>
           </div>
         </div>
