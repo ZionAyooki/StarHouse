e93791ce1bfc21b32048a83a4c4e6146508f009b
Zion Ayooki <zion.ayooki@gmail.com>
2021-01-25 01:21:28 +0200

    Added the services page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import {HashRouter, Switch, Route} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
 import AboutPage from "./pages/AboutPage";
+import ServicesPage from "./pages/ServicesPage";
 
 class App extends React.Component {
   render() {
     return (
       <HashRouter>
         <Switch>
+          <Route exact path="/services">
+            <ServicesPage />
+          </Route>
           <Route exact path="/about">
             <AboutPage />
           </Route>
---
+++ src/pages/ServicesPage.js
@@ -0,0 +1,156 @@
+import React from "react";
+import Header from "../components/header/Header";
+import HeroHeading from "../components/headings/HeroHeading";
+import Footer from "../components/footer/Footer";
+
+class ServicesPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <HeroHeading isLarge={false} title="Services" slogan="Here is a list of the services we provide to our clients." />
+        <br /><br /><br /><br /><br />
+        <section id="service1">
+          <div className="container text-center">
+            <div className="section-title">
+              <h2>Brokerage services</h2>
+              <div className="section-title-divider"></div>
+              <p className="section-slogan">If you're looking to sell or rent your property, we can help.</p>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae inventore
+                  nihil quibusdam. Adipisci beatae explicabo facilis fugiat fugit nihil, sed voluptatibus! Consequatur
+                  et
+                  facilis quis sint unde veritatis.
+                </div>
+                <div>Ab, autem culpa dolorem enim eum exercitationem facere, fuga fugit illo illum, impedit laboriosam
+                  magni maiores mollitia nulla officia perferendis quam quia quisquam similique sit soluta vel vero
+                  voluptatibus voluptatum.
+                </div>
+                <div>Aliquid aperiam asperiores dicta dolor eaque eos error eum excepturi id iure iusto magnam molestiae
+                  mollitia neque nesciunt nihil officia, pariatur quaerat quibusdam quo repellat repellendus soluta
+                  totam
+                  velit voluptatem?
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="service2">
+          <div className="container text-center">
+            <div className="section-title">
+              <h2>Property management</h2>
+              <div className="section-title-divider"></div>
+              <p className="section-slogan">We provide all the needs for managing and maintaining your property.</p>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae inventore
+                  nihil quibusdam. Adipisci beatae explicabo facilis fugiat fugit nihil, sed voluptatibus! Consequatur
+                  et
+                  facilis quis sint unde veritatis.
+                </div>
+                <div>Ab, autem culpa dolorem enim eum exercitationem facere, fuga fugit illo illum, impedit laboriosam
+                  magni maiores mollitia nulla officia perferendis quam quia quisquam similique sit soluta vel vero
+                  voluptatibus voluptatum.
+                </div>
+                <div>Aliquid aperiam asperiores dicta dolor eaque eos error eum excepturi id iure iusto magnam molestiae
+                  mollitia neque nesciunt nihil officia, pariatur quaerat quibusdam quo repellat repellendus soluta
+                  totam
+                  velit voluptatem?
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="service3">
+          <div className="container text-center">
+            <div className="section-title">
+              <h2>Evaluation services</h2>
+              <div className="section-title-divider"></div>
+              <p className="section-slogan">If you need an evaluation for your property.</p>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae inventore
+                  nihil quibusdam. Adipisci beatae explicabo facilis fugiat fugit nihil, sed voluptatibus! Consequatur
+                  et
+                  facilis quis sint unde veritatis.
+                </div>
+                <div>Ab, autem culpa dolorem enim eum exercitationem facere, fuga fugit illo illum, impedit laboriosam
+                  magni maiores mollitia nulla officia perferendis quam quia quisquam similique sit soluta vel vero
+                  voluptatibus voluptatum.
+                </div>
+                <div>Aliquid aperiam asperiores dicta dolor eaque eos error eum excepturi id iure iusto magnam molestiae
+                  mollitia neque nesciunt nihil officia, pariatur quaerat quibusdam quo repellat repellendus soluta
+                  totam
+                  velit voluptatem?
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="service4">
+          <div className="container text-center">
+            <div className="section-title">
+              <h2>Project management</h2>
+              <div className="section-title-divider"></div>
+              <p className="section-slogan">We provide project management services, from the idea to the finished
+                project.</p>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae inventore
+                  nihil quibusdam. Adipisci beatae explicabo facilis fugiat fugit nihil, sed voluptatibus! Consequatur
+                  et
+                  facilis quis sint unde veritatis.
+                </div>
+                <div>Ab, autem culpa dolorem enim eum exercitationem facere, fuga fugit illo illum, impedit laboriosam
+                  magni maiores mollitia nulla officia perferendis quam quia quisquam similique sit soluta vel vero
+                  voluptatibus voluptatum.
+                </div>
+                <div>Aliquid aperiam asperiores dicta dolor eaque eos error eum excepturi id iure iusto magnam molestiae
+                  mollitia neque nesciunt nihil officia, pariatur quaerat quibusdam quo repellat repellendus soluta
+                  totam
+                  velit voluptatem?
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section id="service5">
+          <div className="container text-center">
+            <div className="section-title">
+              <h2>Advisory services</h2>
+              <div className="section-title-divider"></div>
+              <p className="section-slogan">We provide advisory services to home owners.</p>
+            </div>
+            <div className="row">
+              <div className="col-12">
+                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto asperiores beatae inventore
+                  nihil quibusdam. Adipisci beatae explicabo facilis fugiat fugit nihil, sed voluptatibus! Consequatur
+                  et
+                  facilis quis sint unde veritatis.
+                </div>
+                <div>Ab, autem culpa dolorem enim eum exercitationem facere, fuga fugit illo illum, impedit laboriosam
+                  magni maiores mollitia nulla officia perferendis quam quia quisquam similique sit soluta vel vero
+                  voluptatibus voluptatum.
+                </div>
+                <div>Aliquid aperiam asperiores dicta dolor eaque eos error eum excepturi id iure iusto magnam molestiae
+                  mollitia neque nesciunt nihil officia, pariatur quaerat quibusdam quo repellat repellendus soluta
+                  totam
+                  velit voluptatem?
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <br /><br /><br /><br /><br />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default ServicesPage;
